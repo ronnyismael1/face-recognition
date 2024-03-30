3467e2a2af3d8f4bd3a3542a5735246a7adaaed3
Ronny Ismael <ronny.ismael@sjsu.edu>
2024-03-30 11:04:59 -0700

    changed wait time for lock

--- utilities/lock_module.py
+++ utilities/lock_module.py
@@ -7,7 +7,7 @@ GPIO.setup(18, GPIO.OUT)
 
 def unlock_door():
     GPIO.output(18, GPIO.HIGH)  # Assume HIGH signal unlocks the door
-    sleep(5)  # Keep unlocked for 5 seconds
+    sleep(20)  # Keep unlocked for 20 seconds
     lock_door()  # Re-lock the door automatically after 5 seconds
 
 def lock_door():
