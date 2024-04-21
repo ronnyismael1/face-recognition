686d692777fae0ee3d1af33a9128b01081db6090
Michael Zabaneh <michael.zabaneh@sjsu.edu>
2024-04-21 13:02:56 -0700

    final push for realtime integration -Cary

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import threading
 from Flask.Website import create_app  # Importing create_app function
-<<<<<<< HEAD
-import camera.camera_module as camera  # Importing camera module
-
-# Creating Flask app instance using factory function
-=======
 import camera.camera_module as camera  # Importing 
 #Creating Flask app instance using factory function
->>>>>>> 26f58b0f196b561e4986b4d1a92d6e86f7aec2a6
+
 app = create_app()
 
 def run_camera():
